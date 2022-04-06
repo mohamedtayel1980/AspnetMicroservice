30df729dcafb12998b35e19625393306ff88b92c
moh_moh701 <moh.moh701@gmail.com>
2022-04-06 23:19:36 +0200

    implementing  email service

--- src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Ordering.Application.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +21,31 @@ namespace Ordering.Infrastructure.Mail
         {
             _emailSettings = emailSettings.Value;
             _logger = logger;
+        }
+        public async Task<bool> SendEmail(Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+
+            var from = new EmailAddress
+            {
+                Email = _emailSettings.FromAddress,
+                Name = _emailSettings.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            _logger.LogInformation("Email sent.");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+                return true;
+
+            _logger.LogError("Email sending failed.");
+            return false;
         }
     }
 }
